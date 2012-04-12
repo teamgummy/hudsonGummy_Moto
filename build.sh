83f74d34315b209ca6c4cd22b6483f3987977acc
Jason Stewart <sixstringsg@verizon.net>
2012-04-12 18:43:57 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -95,7 +95,6 @@ mka gummy 2>&1 | tee "$LUNCH".log
 
 ZIP=$(tail -2 "$LUNCH".log | cut -f3 -d ' ' | cut -f1 -d ' ' | sed -e '/^$/ d')
 scp out/target/product/$DEVICE/$ZIP website@exynos.co:/home/website/www/gummy.exynos.co/public_html/$DEVICE/$ZIP
-echo zip is at http://cna.exynos.co/$DEVICE/$ZIP Happy flashing!
-check_result Build failed.
 mkdir $WORKSPACE2/archive
 cp out/target/product/$DEVICE/$ZIP $WORKSPACE2/archive
+check_result Build failed.
